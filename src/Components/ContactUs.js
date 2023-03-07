511c1f301200c6fbde08b5f2a95ef8124cf492c3
nida4598 <nidaimtiaz4598@gmail.com>
2023-03-07 09:44:59 +0500

    commit

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,22 +1,40 @@
-import React from 'react';
 import './ContactUs.css';
+import React, { useState } from 'react';
+
 
 function ContactUs() {
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitted(true);
+  };
+
   return (
+    <>
+    <h1>Contact Us</h1>
     <div className="contact-us-container">
-      <br />
-      <h1>Contact Us</h1>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" required />
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" required></textarea>
-        <button type="submit">Send</button>
-      </form>
+      {submitted ? (
+        <p>Thank you for your submission!</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" required></textarea>
+          <button type="submit">Send</button>
+        </form>
+      )}
     </div>
+    </>
   );
 }
 
 export default ContactUs;
+
+
+
+
+
