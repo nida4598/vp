80c228e84cbe3791b54eed28fb11600395eaafff
nida4598 <nidaimtiaz4598@gmail.com>
2023-03-23 19:41:26 +0500

    commit

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Quiz from '../Components/Quiz';
 import ContactUs from '../Components/ContactUs';
+import Header from '../Components/Header';
 
 
 function App() {
   return (
     <>
+    <Header/>
     <Quiz/>
     <ContactUs/>
     </>
